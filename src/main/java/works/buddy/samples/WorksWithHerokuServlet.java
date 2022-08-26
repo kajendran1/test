d27b431f7d694b6c6c650faf636286cbf5605fe8
kajendran1 <103910294+kajendran1@users.noreply.github.com>
2022-08-26 17:13:41 +0530

    Update WorksWithHerokuServlet.java

--- src/main/java/works/buddy/samples/WorksWithHerokuServlet.java
+++ src/main/java/works/buddy/samples/WorksWithHerokuServlet.java
@@ -13,7 +13,7 @@ public class WorksWithHerokuServlet extends HttpServlet {
         response.setContentType("text/plain");
         response.setStatus(404);
         PrintWriter writer = response.getWriter();
-        writer.print("Work with Heroku");
+        writer.print("buddy Work with Heroku");
         writer.close();
     }
 }
